95cc3d46c15f8b4694a62141832eab22c52c81ff
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-04-28 00:04:53 +0900

    ABC

---
+++ AtCorder/src/atcorder/A160.java
@@ -0,0 +1,19 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A160 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+		sc.close();
+		if(S.charAt(2)==S.charAt(3) &&
+			S.charAt(4)==S.charAt(5) ) {
+			System.out.println("Yes");
+		}else {
+			System.out.println("No");
+		}
+	}
+
+}
---
+++ AtCorder/src/atcorder/B160.java
@@ -0,0 +1,17 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class B160 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		sc.close();
+		int s500 = X/500;
+		int s5 = (X%500)/5;
+		int uresi = s500*1000 + s5*5;
+		System.out.println(uresi);
+	}
+
+}
