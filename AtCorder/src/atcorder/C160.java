12468d6388cc947874dbd5ab2c2a7029b407b96a
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-04-28 13:44:09 +0900

    ABC

---
+++ AtCorder/src/atcorder/C160.java
@@ -0,0 +1,30 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class C160 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int K = sc.nextInt();
+		int N = sc.nextInt();
+		int[] A = new int[N];
+		for(int i=0; i<N; i++)
+			A[i] = sc.nextInt();
+		sc.close();
+		int m=0;
+		for(int i=0;i<N-1;i++)
+		{
+			if(i==0)
+			{
+				m=Math.max(m,K-A[N-1]+A[i]);
+			}
+			else
+			{
+				m=Math.max(m,(A[i+1]-A[i]));
+			}
+		}
+		System.out.println(K-m);
+	}
+
+}
