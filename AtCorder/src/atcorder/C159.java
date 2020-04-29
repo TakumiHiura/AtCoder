ff8f1ac4a84a2450bb5c1a9ffd48f262959668e9
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-04-29 15:01:23 +0900

    ABC

---
+++ AtCorder/src/atcorder/C159.java
@@ -0,0 +1,15 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class C159 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double L = sc.nextDouble();
+		sc.close();
+		double l = L/3;
+		System.out.println(Math.pow(l,3));
+	}
+
+}
