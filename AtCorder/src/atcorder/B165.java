4c888789c3c00e787b3655a540fe65d9d816eed9
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-05-04 23:55:53 +0900

    ABC

---
+++ AtCorder/src/atcorder/B165.java
@@ -0,0 +1,20 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class B165 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long X = sc.nextLong();
+		sc.close();
+		long s=100;
+		int k=0;
+		while(s<X) {
+			s+=s/100;
+			k++;
+		}
+		System.out.println(k);
+	}
+
+}
