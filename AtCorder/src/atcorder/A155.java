bf85f227ea19695fa5f50901a759b0771f8afad5
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-05-05 16:14:06 +0900

    ABC155

---
+++ AtCorder/src/atcorder/A155.java
@@ -0,0 +1,20 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A155 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		sc.close();
+		String s = "No";
+		if(a==b && b!=c) s="Yes";
+		if(a==c && b!=c) s="Yes";
+		if(c==b && a!=c) s="Yes";
+		System.out.println(s);
+	}
+
+}
