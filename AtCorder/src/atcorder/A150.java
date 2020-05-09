6a62bffa1d325a08b4c319bf476cdd14df977d06
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-05-09 23:38:43 +0900

    ABC150

---
+++ AtCorder/src/atcorder/A150.java
@@ -0,0 +1,19 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A150 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int K = sc.nextInt();
+		int X = sc.nextInt();
+		sc.close();
+		if(K*500>=X) {
+			System.out.println("Yes");
+		}else {
+			System.out.println("No");
+		}
+	}
+
+}
