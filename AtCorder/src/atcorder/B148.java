98f1eb1029011c4ef682b99039bf2752f0e913a3
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-05-12 22:24:56 +0900

    ABC148

---
+++ AtCorder/src/atcorder/B148.java
@@ -0,0 +1,21 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class B148 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		String S = sc.next();
+		String T = sc.next();
+		sc.close();
+		String U = "";
+		for(int i=0; i<N; i++) {
+			U += S.charAt(i);
+			U += T.charAt(i);
+		}
+		System.out.println(U);
+	}
+
+}
