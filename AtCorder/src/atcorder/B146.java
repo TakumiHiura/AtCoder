ca732ab7290dd98f60535e62dece4d07ab59db38
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-05-15 23:16:20 +0900

    ABC146

---
+++ AtCorder/src/atcorder/B146.java
@@ -0,0 +1,24 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class B146 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		String S = sc.next();
+		String T = "";
+		sc.close();
+		for(int i=0; i<S.length(); i++) {
+			char a = S.charAt(i);
+			a += N;
+			if ('Z' < a) {
+                a -= 26;
+            }
+			T += (char)a;
+		}
+		System.out.println(T);
+	}
+
+}
