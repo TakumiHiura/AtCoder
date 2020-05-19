82f9272e3ab8d6cc2c8d2608bd8e7c729aab1bad
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-05-20 00:02:32 +0900

    ABC144

--- AtCorder/src/atcorder/B150.java
+++ AtCorder/src/atcorder/B150.java
@@ -11,7 +11,7 @@ public class B150 {
 		sc.close();
 		int a = 0;
 		for(int i=0; i<N; i++) {
-			if(S.charAt(i)=='A' && i<=N-2) {
+			if(S.charAt(i)=='A' && i<N-2) {
 				if(S.charAt(i+1)=='B' && S.charAt(i+2)=='C') {
 					a++;
 				}
