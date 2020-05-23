f7ef1d20ed473c95d640065e67d0c7569166e086
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-05-23 14:15:04 +0900

    ABC149

---
+++ AtCorder/src/atcorder/C149.java
@@ -0,0 +1,25 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class C149 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int a = 0;
+		for(int i=X; i<1000000; i++) {
+			int b = 0;
+			for(int j=1; j<i; j++) {
+				if(i%j==0) b++;
+			}
+			if(b<=2) {
+				a = i;
+				break;
+			}
+		}
+		sc.close();
+		System.out.println(a);
+	}
+
+}
