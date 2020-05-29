77beddb4467c128df9904da33d3ecd897a4ce8d6
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-05-29 22:42:54 +0900

    ABC139

---
+++ AtCorder/src/atcorder/A139.java
@@ -0,0 +1,19 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A139 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.next();
+		String T = sc.next();
+		sc.close();
+		int a = 0;
+		for(int i=0; i<3; i++) {
+			if(S.charAt(i) == T.charAt(i)) a++;
+		}
+		System.out.println(a);
+	}
+
+}
