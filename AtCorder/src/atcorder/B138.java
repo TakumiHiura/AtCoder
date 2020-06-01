798560ad30ef2ad459d8bd6223c0158557cfe504
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-06-01 22:41:00 +0900

    ABC138

---
+++ AtCorder/src/atcorder/B138.java
@@ -0,0 +1,22 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class B138 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int a = 0;
+		double b = 0;
+		for(int i=0; i<N; i++) {
+			a = sc.nextInt();
+			b += 1.0/a;
+		}
+		sc.close();
+		double c = 1/b;
+		System.out.println(c);
+
+	}
+
+}
