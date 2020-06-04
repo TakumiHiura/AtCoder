b624b28f31dc85a4cedaa7546f967ccc79aae29d
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-06-04 20:47:00 +0900

    ABC136

---
+++ AtCorder/src/atcorder/A136.java
@@ -0,0 +1,17 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A136 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		sc.close();
+		int D = C-(A-B);
+		System.out.println(D>0?D:0);
+	}
+
+}
