e30a3d58f0b90f796fe7d98b38a064a28377ab37
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-06-09 23:12:55 +0900

    ABC134

---
+++ AtCorder/src/atcorder/A134.java
@@ -0,0 +1,15 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A134 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		sc.close();
+		int s = 3 * r * r;
+		System.out.println(s);
+	}
+
+}
