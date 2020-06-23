69b6c985567df800b5412d2a98fcd5aa6e734d5a
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-06-23 22:50:20 +0900

    ABC127

---
+++ AtCorder/src/atcorder/A127.java
@@ -0,0 +1,18 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A127 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		if(A>12) {
+			System.out.println(B);
+		}else {
+			System.out.println(A>5 ? B/2:0);
+		}
+	}
+
+}
