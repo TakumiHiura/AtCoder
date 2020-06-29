fc9905f429b84cd54c5cb0ff1497d3064f5a7f11
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-06-29 21:36:17 +0900

    ABC125

---
+++ AtCorder/src/atcorder/A125.java
@@ -0,0 +1,17 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A125 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Double A = sc.nextDouble();
+		int B = sc.nextInt();
+		Double T = sc.nextDouble();
+		sc.close();
+		int a = (int)((T+0.5)/A);
+		System.out.println(a*B);
+	}
+
+}
