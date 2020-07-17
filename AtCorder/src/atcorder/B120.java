84ef1fdd5953832303dcc137d1b7ef714fd71eb7
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-07-17 22:56:02 +0900

    ABC120

---
+++ AtCorder/src/atcorder/B120.java
@@ -0,0 +1,28 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class B120 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int K = sc.nextInt();
+		sc.close();
+		int min = Math.min(A, B);
+		int count = 0;
+		int val = 0;
+		for(int i=min; i>=1; i--) {
+			if(A%i==0 && B%i==0) {
+				count++;
+				val = i;
+				if(count==K) {
+					break;
+				}
+			}
+		}
+		System.out.println(val);
+	}
+
+}
