99fddd1a1197fb5b500cb94ab173c99586658b78
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-07-21 23:47:37 +0900

    ABC117

---
+++ AtCorder/src/atcorder/A117.java
@@ -0,0 +1,15 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A117 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double T = sc.nextDouble();
+		double X = sc.nextDouble();
+		sc.close();
+		System.out.println(T/X);
+	}
+
+}
