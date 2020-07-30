c9322ae8ae8705b24474a0b84923353bcea3cd77
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-07-30 22:49:12 +0900

    ABC113

---
+++ AtCorder/src/atcorder/A113.java
@@ -0,0 +1,15 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A113 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		sc.close();
+		System.out.println(X + Y/2);
+	}
+
+}
