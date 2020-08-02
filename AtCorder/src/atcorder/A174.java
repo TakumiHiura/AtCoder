184b9a2a1e202e06216a632f3c20a20539a394d7
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-08-02 22:42:29 +0900

    ABC174

---
+++ AtCorder/src/atcorder/A174.java
@@ -0,0 +1,14 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class A174 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		sc.close();
+		System.out.println(X>=30 ? "Yes":"No");
+	}
+
+}
