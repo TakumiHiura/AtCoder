81ff856fe06392aebf69cb6d37b3aa1eda7c308b
TakumiHiura <takumi_p93@yahoo.co.jp>
2020-08-05 17:09:36 +0900

    ABC111

---
+++ AtCorder/src/atcorder/B111.java
@@ -0,0 +1,21 @@
+package atcorder;
+
+import java.util.Scanner;
+
+public class B111 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int a = 0;
+		for(int i=N; i<1000; i++) {
+			if(i%111==0) {
+				a = i;
+				break;
+			}
+		}
+		sc.close();
+		System.out.println(a);
+	}
+
+}
